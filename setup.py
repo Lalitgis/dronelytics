82c08fad87f25323c1599dad04d7be32ed021169
Lalit BC <89679878+Lalitgis@users.noreply.github.com>
2026-05-30 22:22:02 -0400

    Bump version from 1.0.1 to 1.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="dronelytics",
-    version="1.0.1",
+    version="1.1.1",
     author="Lalit BC",
     description="Drone orthomosaic analysis and agricultural phenotyping",
     long_description=open("README.md", encoding="utf-8").read(),
